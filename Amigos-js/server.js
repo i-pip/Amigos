0c228b790b438ebbba70ec633ccb6d08e9ff9dab
i-pip <phillip.phiri@sap.com>
2019-04-04 09:54:05 +0000

    Changed response to return all user data

--- Amigos-js/server.js
+++ Amigos-js/server.js
@@ -20,13 +20,15 @@ app.use(passport.authenticate("JWT", {
 }));
 
 app.get("/", function (req, res) {
-	res.send("Application User: " + req.user.id);
+	res.send("Application User: " + req.user);
 });
 
 app.get("/users", function (req, res) {
 	//authInfo is attached to req body by @sap/xssec via passport and xssec.JWTSecurity
+	//authInfo actually contains the token passed to this route, which in turn contains scopes / roles etc
 	//more details at https://help.sap.com/viewer/65de2977205c403bbc107264b8eccf4b/Cloud/en-US/4902b6e66cbd42648b5d9eaddc6a363d.html
-	res.status(200).send('Hello');
+
+	res.status(200).send(`Authinfo: ${JSON.stringify(req.authInfo)}`);
 });
 
 app.post("/users", function (req, res) {
